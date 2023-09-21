80af435538a62b77513e1702d30b5012c639e294
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-09-21 00:57:18 -0300

    -----

---
+++ script.js
@@ -0,0 +1,92 @@
+const startBtn = document.querySelector('.start-btn');
+const startBtnIni = document.querySelector('.nav-link-ini')
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.info-btn.sair');
+const sairBtn = document.querySelector('.btn-sairFinal');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continuar');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+startBtnIni.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+sairBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+    
+    showQuestions(0);
+    questionCounter(1);
+}
+
+
+let questionCount = 0;
+let questionNumb = 1;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if(questionCount < questions.length - 1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else
+    {
+        console.log('Questão Completa');
+    }
+}
+
+const optionList = document.querySelector('.option-list');
+
+function showQuestions(index){
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+        <div class="option"><span>${questions[index].options[1]}</span></div> 
+        <div class="option"><span>${questions[index].options[2]}</span></div> 
+        <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelector('.option');
+    for (let i = 0; i < option.length; i++){
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    console.log('userAnswer');
+}
+
+function questionCounter(index){
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
